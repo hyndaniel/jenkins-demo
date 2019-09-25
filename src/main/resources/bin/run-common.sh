7674eade318f67c5679463428962b5aa536d8556
unknown <yining.he@corp.bfd.com>
2019-09-25 16:11:12 +0800

    build.sh

--- src/main/resources/bin/run-common.sh
+++ src/main/resources/bin/run-common.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 RUN_OPE=$1
-MAIN_CLASS="cn.percent.overseas.App"
+MAIN_CLASS="com.glriverside.fkj.message.be.Application"
 
 FILE_NAME=`basename $0`
 cd `dirname $0`
@@ -12,7 +12,7 @@ CONF_DIR=${DEPLOY_DIR}/conf
 LIB_JARS=${DEPLOY_DIR}/lib/*
 
 LOGS_DIR=${DEPLOY_DIR}/logs
-if [[ ! -d ${LOGS_DIR} ]]; then
+if [ ! -d ${LOGS_DIR} ]; then
 	mkdir ${LOGS_DIR}
 fi
 
@@ -20,45 +20,24 @@ STDOUT_FILE=${LOGS_DIR}/stdout.log
 
 start_service(){
   local SCRIPT_NAME="${BIN_DIR}/${FILE_NAME}"
-  local SCRIPT_USER=`ls -lrt ${SCRIPT_NAME} |awk '{ print $3}'`
-  local OPE_USER=`whoami`
 
-  if [[ ${OPE_USER} == "root" ]] ; then
-      echo "吓死了，您竟然用root来启动程序，程序已自杀!!!!";
-      exit 1
-  fi
+  local JAVA_OPTS=" -Xloggc:gc_memory_logs.log -XX:+PrintGCDetails -Xmx16g -Xms1g  -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled  -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
 
-  if [[ ${SCRIPT_USER} != "$OPE_USER" ]]; then
-      echo "程序启动用户和脚本文件所有者不一致，请确定启动用户是否合法!!!!";
-      exit 1
-  fi
+  echo -e "Starting the service ...\c"
 
-  # |grep "$CONF_DIR"
-  local PIDS=`ps  --no-heading -C java -f --width 1000 | grep "$MAIN_CLASS" |awk '{print $2}'`
+  echo "java $JAVA_OPTS -Ddruid.logType=slf4j -classpath $CONF_DIR:$LIB_JARS $MAIN_CLASS"
 
-  if [[ -n "$PIDS" ]]; then
-    echo "ERROR: The service already started!"
-    echo "PID: $PIDS"
-    exit 1
-  fi
-
-  # local PIDS=`ps  --no-heading -C java -f --width 1000 |grep "$CONF_DIR"| grep "$MAIN_CLASS" |awk '{print $2}'`
-  local JAVA_OPTS=" -Xloggc:gc_memory_logs.log -XX:+PrintGCDetails -Xmx512m -Xms512m  -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
-
-  echo "Starting the service ..."
-
-  java ${JAVA_OPTS} -Ddruid.logType=slf4j -classpath ${CONF_DIR}:${LIB_JARS} ${MAIN_CLASS} 
+  java $JAVA_OPTS -Ddruid.logType=slf4j -classpath ${CONF_DIR}:${LIB_JARS} ${MAIN_CLASS}
 
   echo "OK!"
-  PIDS=`ps  --no-heading -C java -f --width 1000 | grep "$DEPLOY_DIR" | awk '{print $2}'`
-  echo "PID: $PIDS"
+  echo "STDOUT: $STDOUT_FILE"
 }
 
 stop_service(){
-  local PIDS=`ps  --no-heading -C java -f --width 1000 | grep "$MAIN_CLASS" |awk '{print $2}'`
+  local PIDS=`ps  --no-heading -C java -f --width 1000 |grep "$CONF_DIR"| grep "$MAIN_CLASS" |awk '{print $2}'`
 
   if [ -z "$PIDS" ]; then
-    echo "Warning: The service does not started!"
+    echo "ERROR: The service does not started!"
     exit 1
   fi
 
